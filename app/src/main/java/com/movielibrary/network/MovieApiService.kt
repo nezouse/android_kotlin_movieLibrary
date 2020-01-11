e4f26376cdeb513fc3422fd09e47cb335ee0475b
BarTar213 <bartlomiej.tarczynski1@gmail.com>
2020-01-11 19:07:57 +0100

    Added service to fetch data from api

---
+++ app/src/main/java/com/movielibrary/network/MovieApiService.kt
@@ -0,0 +1,34 @@
+package com.movielibrary.network
+
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import kotlinx.coroutines.Deferred
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+private const val BASE_URL = "https://api.themoviedb.org/3/movie/"
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .addCallAdapterFactory(CoroutineCallAdapterFactory())
+    .baseUrl(BASE_URL)
+    .build()
+
+interface MovieApiService {
+
+    @GET("popular")
+    fun getPropertiesAsync(@Query("api_key") type: String):
+            Deferred<MoviesResult>
+
+}
+
+object MovieApi {
+    val retrofitService : MovieApiService by lazy { retrofit.create(MovieApiService::class.java) }
+}
--- app/src/main/java/com/movielibrary/ui/main/MainViewModel.kt
+++ app/src/main/java/com/movielibrary/ui/main/MainViewModel.kt
@@ -1,7 +1,25 @@
 package com.movielibrary.ui.main
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.movielibrary.network.MovieApi
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
     // TODO: Implement the ViewModel
+    private var viewModelJob = Job()
+
+
+    private val coroutineScope = CoroutineScope(viewModelJob + Dispatchers.Main)
+    fun getPopularMovies() {
+        coroutineScope.launch {
+            val getPropertiesDeferred =
+                MovieApi.retrofitService.getPropertiesAsync("API_KEY")
+            val listResult = getPropertiesDeferred.await().movieList
+            Log.i("INFO", listResult.toString())
+        }
+    }
 }
